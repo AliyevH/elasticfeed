638f0b537d870c1db3fc935904f0223475df8651
Hasan Aliyev <hasan@onneks.com>
2020-09-10 19:16:25 +0400

    commands modified

--- elastic_feeder/scripts/commands.py
+++ elastic_feeder/scripts/commands.py
@@ -7,10 +7,10 @@ def cli():
     """ Group cli"""
 
 
-@cli.command(help="test")
-@click.option("--host", default="localhost", help="Specify Elasticsearch host")
-@click.option("--port", default="9200", help="Specify Elasticsearch port")
-@click.option("--index", required=True, help="Specify Index name")
+@cli.command(help="Example: elasticfeed --host 127.0.0.1 --port 9200 <full filepath>")
+@click.option("--host", default="localhost", help="Specify Elasticsearch host", prompt="[default: 127.0.0.1]")
+@click.option("--port", default="9200", help="Specify Elasticsearch port", prompt="[default: 9200]")
+@click.option("--index", required=True, help="Specify Index name", prompt="csv")
 @click.argument('filename', type=click.Path(exists=True))
 def command(host, port, filename, index):
     fe = FeedElastic(host, port, filename, index)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elasticfeed",
-    version="1.0.3",
+    version="1.0.4",
     include_package_data=True,
     packages=find_packages(),
     author="Hasan Aliyev",
